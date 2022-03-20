aa897890d23b7e8ae86aa6bc89abd9094150899f
Andrew94www <popelnucha70@gmail.com>
2022-03-21 01:51:57 +0200

    fix commit

--- laravel/app/Http/Controllers/ZohoController.php
+++ laravel/app/Http/Controllers/ZohoController.php
@@ -5,33 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class ZohoController extends Controller
 {
-    public function auth(Request $request)
-    {
+    public function auth()
+    {   $clientid = '1000.BG2CQW3UUOM34RYN0SUCJR86SKMXVM';
         $uri = route('zohocrm');
-        $scope =  'ZohoInvoice.deals.Create';
-        $clientid = 'here your zoho clientid';
+        $scope =  'ZohoCRM.modules.deals.CREATE';
         $accestype = 'offline';
-
         $redirectTo = 'https://accounts.zoho.com/oauth/v2/auth' . '?' . http_build_query(
         [
         'client_id' => $clientid,
         'redirect_uri' => $uri,
-        'scope' => 'ZohoInvoice.contacts.Create',
+        'scope' =>'ZohoCRM.modules.deals.CREATE',
         'response_type' => 'code',
         ]);
 
         return redirect($redirectTo);
     }
 
-    public function store(Request $request)
+    public function store()
     {
-        $input = $request->all();
         $client_id = '1000.BG2CQW3UUOM34RYN0SUCJR86SKMXVM';
         $client_secret = 'cb9560795cd52558f8c087a5be5049139cdd62c0f4';
        
 
-        // Get ZohoCRM Token
-        $tokenUrl = 'https://accounts.zoho.com/oauth/v2/token?code='.$input["code"].'&client_id='.$client_id.'&client_secret='.$client_secret.'&redirect_uri='.route('zohocrm').'&grant_type=authorization_code';
+        $tokenUrl = 'https://accounts.zoho.com/oauth/v2/token?code=&client_id='.$client_id.'&client_secret='.$client_secret.'&redirect_uri='.route('zohocrm').'&grant_type=authorization_code';
 
         $tokenData = [
 
@@ -50,11 +46,8 @@ class ZohoController extends Controller
         $tResult = curl_exec($curl);
         curl_close($curl);
         $tokenResult = json_decode($tResult);
-        // dd($tokenResult->);
         if(isset($tokenResult->access_token) && $tokenResult->access_token != '') {
       
-
-            // Add Contact in ZohoCRM
             $jsonData = '{
                 "Deal_Name": Task,
                 "Subject":Deskription
@@ -65,15 +58,14 @@ class ZohoController extends Controller
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);     
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);     
             curl_setopt($curl, CURLOPT_TIMEOUT, 300);   
-            curl_setopt($curl, CURLOPT_POST, TRUE);//Regular post  
+            curl_setopt($curl, CURLOPT_POST, TRUE);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);     
             curl_setopt($curl, CURLOPT_HTTPHEADER, array(
                 "Authorization: Zoho-oauthtoken ".$tokenResult->access_token,
-                "X-com-zoho-invoice-organizationid: 688931512"
             ) );
             curl_setopt($curl, CURLOPT_POSTFIELDS,'JSONString='.$jsonData);
             
-            //Execute cUrl session
+        
             $cResponse = curl_exec($curl);
             curl_close($curl);
 
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ZohoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('zohocrmauth', [ZohoController::class, 'auth'])->name('zohocrmauth');
+Route::get('zohocrm', [ZohoController::class, 'store'])->name('zohocrm');
